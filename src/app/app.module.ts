c91401a08be7b562b5d734a4481961e2c6495561
ImedItescia <imededdine.fodeilla@edu.itescia.fr>
2020-10-22 14:11:25 +0200

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,8 @@ import { Routes, RouterModule } from '@angular/router';
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'slides/form', component: FormComponent },
-  { path: 'slides/slider', component: SliderComponent },
+  { path: 'slider/form', component: FormComponent },
+  { path: 'slider/slide', component: SliderComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home' }
 ];
